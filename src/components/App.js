57add458b18ceaf3540605440796ff8aa5abe0d0
sara toftegaard <satoftegaard@gmail.com>
2017-01-11 21:43:58 -0500

    starting point

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    return <body>
+      <header>
+        <h1>Form over Function</h1>
+      </header>
+      <main>
+        <p>CSS Tricks</p>
+        <div className='hover-box' />
+      </main>
+    </body>
   }
 }
 
